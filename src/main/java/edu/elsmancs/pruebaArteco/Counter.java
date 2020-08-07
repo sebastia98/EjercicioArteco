d60408afefa8e4657e1d848f5e49d5be04fdf88f
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-08-07 16:31:58 +0200

    Feat: Crear Counter y su constructor

---
+++ src/main/java/edu/elsmancs/pruebaArteco/Counter.java
@@ -0,0 +1,13 @@
+package edu.elsmancs.pruebaArteco;
+
+public class Counter {
+
+	String loren = null;
+	
+	public Counter(String loren) {
+		// TODO Auto-generated constructor stub
+		this.loren = loren;
+	}
+	
+
+}
