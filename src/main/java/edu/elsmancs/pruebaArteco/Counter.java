33134c4ff54a24e44766019fc1d9cfd547532ad7
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-08-07 18:25:32 +0200

    Feat: Implementar método countPhrases

--- src/main/java/edu/elsmancs/pruebaArteco/Counter.java
+++ src/main/java/edu/elsmancs/pruebaArteco/Counter.java
@@ -19,13 +19,30 @@ public class Counter {
 			return 0;
 		}
 		
-		// Iniciamos en un para tener en cuenta la primera palabra
+		// Iniciamos en uno el contador para tener en cuenta la primera palabra
 		int contador = 1;
 		
 		for (int i = 0; i < loren.length; i ++) {
 			if (loren[i] == ' ' && loren[i + 1] != ' ') { 
 				contador ++;
 			} else if (loren[i] == '\n' && loren[i + 1] == '\n'){
+				contador ++;
+			}
+		}
+		return contador;
+	}
+
+	public int countPhrases() {
+		// TODO Auto-generated method stub
+		
+		if (loren.length == 0) {
+			return 0;
+		}
+		
+		int contador = 0;
+		
+		for (int i = 0; i < loren.length; i ++) {
+			if (loren[i] == '.') {
 				contador ++;
 			}
 		}
