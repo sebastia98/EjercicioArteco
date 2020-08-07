b2c1e33ead7334e12b99ddc89b813f21a7d7459a
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-08-07 21:37:35 +0200

    Refactor: Dividir codigo en dos clases, Counter y Converser

---
+++ src/main/java/edu/elsmancs/pruebaArteco/Converser.java
@@ -0,0 +1,44 @@
+package edu.elsmancs.pruebaArteco;
+
+import java.util.ArrayList;
+
+public class Converser {
+	String lorenString = null;
+	private char[] lorenArrayChar = null;
+	private ArrayList<String> arrayListLoren = new ArrayList<String>();
+	
+	public Converser(String loren) {
+		this.lorenString = loren;
+	}
+	
+	public void setArrayList(String loren) {
+		
+		String palabra = "";
+		
+		for (int i = 0; i < loren.length(); i++) {
+			
+			if (loren.charAt(i) != ' ' && loren.charAt(i) != '.' &&
+				loren.charAt(i) != ',' && loren.charAt(i) != '\n') {
+				palabra = palabra + loren.charAt(i);
+
+			} else {
+				if (palabra != "") {
+					arrayListLoren.add(palabra);
+					palabra = "";
+				}
+			}
+		}
+	}
+
+	public ArrayList<String> getArrayList(){
+		return arrayListLoren;
+	}
+	
+	public void setArrayChar(String loren) {
+		this.lorenArrayChar = loren.toCharArray();
+	}
+	
+	public char[] getArrayChar() {
+		return this.lorenArrayChar;
+	}
+}
--- src/main/java/edu/elsmancs/pruebaArteco/Counter.java
+++ src/main/java/edu/elsmancs/pruebaArteco/Counter.java
@@ -1,62 +1,31 @@
 package edu.elsmancs.pruebaArteco;
 
-import java.util.ArrayList;
-
 public class Counter {
 	
-	String lorenString = null;
-	private char[] lorenArrayChar = null;
-	private ArrayList<String> arrayListLoren = new ArrayList<String>();
+	String loren = null;
+	
+	//Inyección de dependencias
+	
+	private Converser conversor = new Converser(loren);
 	
 	public Counter(String loren) {
-		this.lorenString = loren;
-	}
-	
-	private void setArrayList(String loren) {
-		
-		String palabra = "";
-		
-		for (int i = 0; i < loren.length(); i++) {
-			
-			if (loren.charAt(i) != ' ' && loren.charAt(i) != '.' &&
-				loren.charAt(i) != ',' && loren.charAt(i) != '\n') {
-				palabra = palabra + loren.charAt(i);
-
-			} else {
-				if (palabra != "") {
-					arrayListLoren.add(palabra);
-					palabra = "";
-				}
-			}
-		}
-	}
-	
-	private void setArrayChar(String loren) {
-		this.lorenArrayChar = loren.toCharArray();
-	}
-	
-	public char[] getArrayChar() {
-		return this.lorenArrayChar;
-	}
-	
-	public ArrayList<String> getArrayList(){
-		return arrayListLoren;
+		this.loren = loren;
 	}
 	
 	public int countWords() {
-		setArrayList(this.lorenString);
-		return getArrayList().size();
+		conversor.setArrayList(this.loren);
+		return conversor.getArrayList().size();
 	}
 	
 	public int countPhrases() {
 		// TODO Auto-generated method stub
 		
-		setArrayChar(this.lorenString);
+		conversor.setArrayChar(this.loren);
 		
 		int contador = 0;
 		
-		for (int i = 0; i < getArrayChar().length; i ++) {
-			if (getArrayChar()[i] == '.') {
+		for (int i = 0; i < conversor.getArrayChar().length; i ++) {
+			if (conversor.getArrayChar()[i] == '.') {
 				contador ++;
 			}
 		}
