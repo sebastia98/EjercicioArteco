a95809d940372a7b2283f3a4ed1d98b9fd52df17
sr_Adrover <sadrover@cifpfbmoll.eu>
2020-08-07 17:48:23 +0200

    Feat: Implementar método contarPalabras

--- src/main/java/edu/elsmancs/pruebaArteco/Counter.java
+++ src/main/java/edu/elsmancs/pruebaArteco/Counter.java
@@ -1,12 +1,27 @@
 package edu.elsmancs.pruebaArteco;
 
+
 public class Counter {
 
-	String loren = null;
+	private char[] loren = null;
 	
 	public Counter(String loren) {
 		// TODO Auto-generated constructor stub
-		this.loren = loren;
+		this.loren = loren.toCharArray();
+	}
+
+	public int countWords() {
+		// TODO Auto-generated method stub
+		int contador = 1;
+		
+		for (int i = 0; i < loren.length; i ++) {
+			if (loren[i] == ' ' && loren[i + 1] != ' ') { 
+				contador ++;
+			} else if (loren[i] == '\n' && loren[i + 1] == '\n'){
+				contador ++;
+			}
+		}
+		return contador;
 	}
 	
 
